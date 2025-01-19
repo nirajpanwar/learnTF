2bbad0aa1aa7da0c80b84d05d817bcf79d25bdaa
nirajpanwar <npanwar@covalience.io>
2025-01-19 20:57:27 +0530

    Add files via upload

---
+++ terraform.tfvars
@@ -0,0 +1,5 @@
+bucket_name      = "bckt-neeraj"
+aws_region       = "us-east-1"
+enable_versioning = true
+environment      = "production"
+owner            = "team-devops"
---
+++ variable.tf
@@ -0,0 +1,28 @@
+variable "aws_region" {
+  description = "The AWS region to deploy resources"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "bucket_name" {
+  description = "The name of the S3 bucket"
+  type        = string
+}
+
+variable "enable_versioning" {
+  description = "Enable versioning on the S3 bucket"
+  type        = bool
+  default     = true
+}
+
+variable "environment" {
+  description = "The environment for deployment (e.g., dev, staging, prod)"
+  type        = string
+  default     = "prod"
+}
+
+variable "owner" {
+  description = "Owner of the resources"
+  type        = string
+  default     = "team-devops"
+}
