83d5ec455326c1025dfa7da3abd051d0c2549ab3
nirajpanwar <npanwar@covalience.io>
2025-01-20 11:20:05 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,1 +1,9 @@
 
+ terraform {
+  backend "s3" {
+    bucket         = "bckt-neeraj"  # Use the created bucket name
+    key            = "prod/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+  }
+ }
